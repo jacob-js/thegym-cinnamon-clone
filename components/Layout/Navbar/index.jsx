6a1c3c17c783eef2ca7701e3cd006603fad13ef5
Jacob Dev <mercihabam@gmail.com>
2022-11-01 14:34:06 +0200

    add hero section

--- components/Layout/Navbar/index.jsx
+++ components/Layout/Navbar/index.jsx
@@ -3,17 +3,17 @@ import Button from '../../ui/Button'
 
 function Navbar() {
   return (
-    <div className='navbar fixed w-full top-0 left-0 py-5 bg-black bg-opacity-90 bg-noise text-white font-semibold'>
+    <div className='navbar fixed w-full top-0 left-0 py-5 bg-opacity-90 z-30 text-white font-semibold'>
       <div className="content flex justify-between items-center">
         <img src="/app/logo-white.svg" alt="" className='h-5' />
         <div className="flex gap-8 items-center">
-          <div className="hover:text-primary transition-all">Projects</div>
-          <div className="hover:text-primary transition-all">Services</div>
-          <div className="hover:text-primary transition-all">About Us</div>
-          <div className="hover:text-primary transition-all">Careers</div>
-          <div className="hover:text-primary transition-all">Blog</div>
-          <div className="w-11.5 h-11.5 rounded-full border-2 border-white flex justify-center items-center">
-            <i class="fa-sharp fa-solid fa-play text-white text-lg"></i>
+          <div className="hover:text-primary transition-all cursor-pointer">Projects</div>
+          <div className="hover:text-primary transition-all cursor-pointer">Services</div>
+          <div className="hover:text-primary transition-all cursor-pointer">About Us</div>
+          <div className="hover:text-primary transition-all cursor-pointer">Careers</div>
+          <div className="hover:text-primary transition-all cursor-pointer">Blog</div>
+          <div className="w-11.5 h-11.5 rounded-full border-2 border-white flex justify-center items-center cursor-pointer transition-all hover:border-primary hover:bg-primary hover:text-white">
+            <i class="fa-sharp fa-solid fa-play text-white text-lg "></i>
           </div>
           <Button>Contact us</Button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        '160': '40rem',
         '11.5': '2.875rem',
         '12/10': '120%'
+      },
+      maxWidth: {
+        '160': '40rem',
       },
       backgroundImage: {
         'noise': "url('https://cinnamon.agency/images/noise.png')"
@@ -18,7 +22,12 @@ module.exports = {
         'white': 'rgb(255 255 255) 0px 0px 0px 155px inset'
       },
       colors: {
-        'primary': 'rgb(81, 53, 255)'
+        'primary': 'rgb(81, 53, 255)',
+        'light': 'rgb(207, 207, 207)'
+      },
+      fontSize: {
+        '20': '5rem',
+        '22': '5.5rem'
       }
     },
   },
